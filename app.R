c3edc7214031affcfc029c2f54c8d01185afeeba
ialurod <ian.rodriguez@estudiantat.upc.edu>
2026-04-15 11:49:15 +0200

    app.R

--- app.R
+++ app.R
@@ -25,7 +25,6 @@ my_colors <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00",
                "#FFFF33", "#A65628", "#F781BF", "#999999", "#66C2A5", 
                "#FC8D62", "#8DA0CB")
 
-# --- 2. SHINY UI ---
 ui <- fluidPage(
   tags$head(
     tags$style(HTML("
